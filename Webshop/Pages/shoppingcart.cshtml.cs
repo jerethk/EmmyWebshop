9f0639a0a680a28c95252aed1e0b78bb769d12ae
jerethk <82040941+jerethk@users.noreply.github.com>
2022-05-30 01:04:49 +1000

    Update shopping cart

--- Webshop/Pages/shoppingcart.cshtml.cs
+++ Webshop/Pages/shoppingcart.cshtml.cs
@@ -12,14 +12,76 @@ namespace Webshop.Pages
 {
     public class shoppingcartModel : PageModel
     {
+        [FromQuery]
+        public string action { get; set; }
+
+        [FromQuery]
+        public string product { get; set; }
+        
         public List<ShoppingCartItem> cartItems { get; set; }
+        public decimal cartTotal { get; set; }
 
         public async Task<IActionResult> OnGetAsync()
         {
             string cartJSON = HttpContext.Session.GetString("cart");
-            cartItems = JsonSerializer.Deserialize<List<ShoppingCartItem>>(cartJSON);
+            
+            if (cartJSON != null)
+            {
+                cartItems = JsonSerializer.Deserialize<List<ShoppingCartItem>>(cartJSON);
+
+                // add item
+                if (action == "add")
+                {
+                    foreach (ShoppingCartItem item in cartItems)
+                    {
+                        if (item.product.ProductCode == this.product)
+                        {
+                            item.count += 1;
+                            break;
+                        }
+                    }
+
+                    updateCart();
+                }
+
+                // remove item
+                if (action == "remove")
+                {
+                    foreach (ShoppingCartItem item in cartItems)
+                    {
+                        if (item.product.ProductCode == this.product)
+                        {
+                            item.count -= 1;
+
+                            if (item.count == 0)
+                            {
+                                cartItems.Remove(item);
+                            }
+                            
+                            break;
+                        }
+                    }
+
+                    updateCart();
+                }
+
+                // calculate total price of cart
+                cartTotal = 0;
+                foreach (ShoppingCartItem item in cartItems)
+                {
+                    decimal price = item.product.Price * item.count;
+                    cartTotal += price;
+                }
+            }
 
             return Page();
+        }
+
+        private void updateCart()
+        {
+            // reserialise and store in session
+            string cartJSON = JsonSerializer.Serialize(cartItems);
+            HttpContext.Session.SetString("cart", cartJSON);
         }
     }
 
