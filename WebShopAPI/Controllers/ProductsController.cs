5e5669e69a8c127cdeaf7777e61485dba2b07e18
jerethk <82040941+jerethk@users.noreply.github.com>
2022-06-12 01:13:41 +1000

    Start some preliminary work

---
+++ WebShopAPI/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Webshop.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Webshop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private myshopContext _shopContext;
+
+        // GET: api/<ProductsController>
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            _shopContext = new myshopContext();
+
+            List<Product> productList = (from Product p in _shopContext.Products
+                                         select p).ToList();
+
+            return productList;
+        }
+
+    }
+}
